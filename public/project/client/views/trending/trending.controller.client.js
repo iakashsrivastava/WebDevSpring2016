7e8d6a32b39b9adfbcbcee377a19a1c3b55b4e25
iakashsrivastava <iakashsrivastava@gmail.com>
2016-04-19 12:34:23 -0400

    Back button on trending page

--- public/project/client/views/trending/trending.controller.client.js
+++ public/project/client/views/trending/trending.controller.client.js
@@ -72,15 +72,15 @@
 
         }
 
-        angular.element($window).bind("scroll", function() {
-            var windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
-            var body = document.body, html = document.documentElement;
-            var docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight,  html.scrollHeight, html.offsetHeight);
-            windowBottom = windowHeight + window.pageYOffset;
-            if (windowBottom >= docHeight) {
-                loadTrends();
-            }
-        });
+        //angular.element($window).bind("scroll", function() {
+        //    var windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+        //    var body = document.body, html = document.documentElement;
+        //    var docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight,  html.scrollHeight, html.offsetHeight);
+        //    windowBottom = windowHeight + window.pageYOffset;
+        //    if (windowBottom >= docHeight) {
+        //        loadTrends();
+        //    }
+        //});
 
         function gotoURL(url){
 
