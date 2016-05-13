c7923ed8126032e2d088fd0a6db653ef1118a353
iakashsrivastava <iakashsrivastava@gmail.com>
2016-05-12 23:46:46 -0400

    Working on creation of portfolio page

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 //require('./public/assignment/server/app.js')(app);
-require('./public/assignment/server/app.js')(app, db, mongoose);
-//require('./public/project/server/app.js')(app, db, mongoose);
+//require('./public/assignment/server/app.js')(app, db, mongoose);
+require('./public/project/server/app.js')(app, db, mongoose);
 
 app.get('/hello', function(req, res){
     res.send('hello world from webstorm');
